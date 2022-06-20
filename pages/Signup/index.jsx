04606d9dbaa0761ce09944225b164163829110fa
jorddo <jordanbsneed@gmail.com>
2022-06-20 18:04:44 -0500

    setting up supabase

---
+++ pages/Signup/index.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Signup = () => (
+  <h1>
+    This is the signup page, bb!
+    <Link href='/'> Click here to go home.</Link>
+  </h1>
+);
+
+export default Signup;
