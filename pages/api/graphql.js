172b392f9a4a771e1402ba62093f7c720e2c2696
jorddo <jordanbsneed@gmail.com>
2022-06-30 18:39:13 -0500

    adding supabase to app

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -38,7 +38,7 @@ const server = new ApolloServer({
 });
 
 server
-  .listen({ port: process.env.PORT || 4000 })
+  .listen({ port: process.env.PORT || 3000 })
   .then(({ url }) =>
     console.log(`🚀 Server ready at: ${url}${graphqlEndpoint}`)
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { supabase } from 'utils/supabaseClient';
 
 export default function Home() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
     <div>
       <Head>
